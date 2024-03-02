65a72d7636e1572210477ea8f35886d17131a02d
Jose Ibanez <jocibanez1904@gmail.com>
2024-03-02 04:22:28 -0500

    Crea la logica para el funcionamiento del des-encriptador

---
+++ script.js
@@ -0,0 +1,87 @@
+// Variables
+let textoGuardado;
+let textoParaEncriptar;
+let textoParaDesencriptar;
+let textoEncriptado = "";
+let textoDesencriptado = "";
+let areaEncriptado = "";
+let areaDesencriptado = "";
+
+// Seleccionar los elementos textarea por su ID
+areaEncriptado = document.getElementById('area-desencriptar');
+areaDesencriptado = document.getElementById('area-encriptar');
+
+// Agregar listeners para cambiar el color de fondo al hacer clic en los textareas
+areaEncriptado.addEventListener('click', function() {this.style.background = "white";})
+areaDesencriptado.addEventListener('click', function() {this.style.background = "white";})
+
+// Función para guardar el texto y determinar si encriptar o desencriptar
+function guardarTexto(areaId) {
+    textoGuardado = document.getElementById(areaId).value;
+    
+    // Verificar si el textarea no está vacío
+    if (textoGuardado !== "") {
+        areaEncriptado.style.background = "white";
+        areaDesencriptado.style.background = "white";
+    }
+
+    // Determinar si se va a encriptar o desencriptar según el área
+    if (areaId == "area-desencriptar") {
+        desencriptarTexto();
+    } else {
+        encriptarTexto();
+    }
+}
+
+// Función para encriptar el texto
+function encriptarTexto() {
+    for (let index = 0; index < textoGuardado.length; index++) {
+        textoParaEncriptar = textoGuardado[index];
+        if (textoParaEncriptar == 'a') {
+            textoEncriptado += "ai";
+        } else if (textoParaEncriptar == 'e') {
+            textoEncriptado += "enter";
+        } else if (textoParaEncriptar == 'i') {
+            textoEncriptado += "imes";
+        } else if (textoParaEncriptar == 'o') {
+            textoEncriptado += "ober";    
+        } else if (textoParaEncriptar == 'u') {
+            textoEncriptado += "ufat";    
+        } else {
+            textoEncriptado += textoParaEncriptar;
+        }
+    }
+
+    // Establecer el texto encriptado en el textarea correspondiente
+    areaEncriptado.value = textoEncriptado;
+    textoEncriptado = ""; // Reiniciar la variable para el próximo uso
+}
+
+// Función para desencriptar el texto
+function desencriptarTexto() {
+    for (let index = 0; index < textoGuardado.length; index++) {
+        textoParaDesencriptar = textoGuardado[index];
+        if (textoParaDesencriptar == 'a') {
+            textoDesencriptado += "a";
+            index++;
+        } else if (textoParaDesencriptar == 'e') {
+            textoDesencriptado += "e";
+            index += 4;
+        } else if (textoParaDesencriptar == 'i') {
+            textoDesencriptado += "i";
+            index += 2;
+        } else if (textoParaDesencriptar == 'o') {
+            textoDesencriptado += "o";
+            index += 3;    
+        } else if (textoParaDesencriptar == 'u') {
+            textoDesencriptado += "u";
+            index += 3;    
+        } else {
+            textoDesencriptado += textoParaDesencriptar;
+        }
+    }
+
+    // Establecer el texto desencriptado en el textarea correspondiente
+    areaDesencriptado.value = textoDesencriptado;
+    textoDesencriptado = ""; // Reiniciar la variable para el próximo uso
+}
